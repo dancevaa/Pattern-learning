add1833b5a45b8a2fbeeb325b8b03628bc66d857
Ann Ann <annann@192.168.100.5>
2023-02-15 23:57:41 +0300

    Initial commit

---
+++ src/main/java/com/danceva/patternlearning/PatternLearningApplication.java
@@ -0,0 +1,13 @@
+package com.danceva.patternlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatternLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatternLearningApplication.class, args);
+	}
+
+}
